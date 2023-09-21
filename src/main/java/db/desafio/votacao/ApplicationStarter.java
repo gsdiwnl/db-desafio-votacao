11103252c44f147dc95eb45a320e0f80fe5e060f
Gabriel Dullius <gd@interact.com.br>
2023-09-21 16:26:54 -0300

    init project

---
+++ src/main/java/db/desafio/votacao/ApplicationStarter.java
@@ -0,0 +1,31 @@
+/**
+ * Filename:    ApplicationStarter.java
+ *
+ * Description: Implementation of the ApplicationStarter class.
+ *
+ * Revision:    1.0
+ *
+ * Changed by:  gd
+ *
+ * Author:      Gabriel Dullius
+ * Email:       dulliusgabriel@gmail.com
+ *
+ * Copyright of the computer program(s) contained herein
+ * is of intellectual property of the studying for the voting challenge by DB.
+ * The program(s) may be used and/or copied with permission.
+ * Challenge: https://github.com/dbserver/desafio-votacao
+ * 
+ */
+package db.desafio.votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStarter 
+{
+	public static void main( String[] args )
+	{
+		SpringApplication.run( ApplicationStarter.class, args );
+	}
+}
