d0c518f825fa41d545ab5f04a3a34b4601722dc1
Gabriel Dullius <gd@interact.com.br>
2023-09-22 14:48:25 -0300

    feat: added Swagger example

---
+++ src/main/java/com/db/desafio/votacao/api/v1/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+/**
+ * Filename:    SwaggerConfig.java
+ *
+ * Description: Implementation of the SwaggerConfig class.
+ *
+ * Revision:    1.0
+ *
+ * Changed by:  gd
+ *
+ * Author:      Gabriel Dullius
+ * Email:       dulliusgabriel@gmail.com
+ *
+ * Copyright of the computer program(s) contained herein
+ * is of intellectual property of the studying for the voting challenge by DB.
+ * The program(s) may be used and/or copied with permission.
+ * Challenge: https://github.com/dbserver/desafio-votacao
+ *
+ */
+package com.db.desafio.votacao.api.v1.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig 
+{
+    @Bean
+    OpenAPI apiInfo() 
+    {
+        return new OpenAPI()
+                .info( new Info()
+                        .title( "Desafio votação" )
+                        .description("API utilizada para resolução do Desafio de Votação em SpringBoot")
+                        .contact( new Contact()
+                                    .name( "Gabriel Dullius" )
+                                    .url( "http://github.com/gsdiwnl" ))
+                                    .version("1.0.0")
+                                    .license( new License() )
+                                    .termsOfService("") )
+                .externalDocs( new ExternalDocumentation()
+                                .description("Repositório da API no GitHub")
+                                .url("https://github.com/gsdiwnl/db-desafio-votacao"))
+                                .addServersItem( new Server()
+                                                    .url( "http://localhost:8080/v1") );
+    }
+}
---
+++ src/main/java/com/db/desafio/votacao/api/v1/modules/votacaoAssembleia/controllers/swagger/PautaSwagger.java
@@ -0,0 +1,80 @@
+/**
+ * Filename:    PautaSwagger.java
+ *
+ * Description: Implementation of the PautaSwagger class.
+ *
+ * Revision:    1.0
+ *
+ * Changed by:  gd
+ *
+ * Author:      Gabriel Dullius
+ * Email:       dulliusgabriel@gmail.com
+ *
+ * Copyright of the computer program(s) contained herein
+ * is of intellectual property of the studying for the voting challenge by DB.
+ * The program(s) may be used and/or copied with permission.
+ * Challenge: https://github.com/dbserver/desafio-votacao
+ *
+ */
+package com.db.desafio.votacao.api.v1.modules.votacaoAssembleia.controllers.swagger;
+
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.db.desafio.votacao.api.v1.misc.Error;
+import com.db.desafio.votacao.api.v1.modules.votacaoAssembleia.database.models.Pauta;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+public interface PautaSwagger 
+{
+    final String TAG_NAME = "Pauta";
+
+    @Operation(
+        operationId = "buscar pautas",
+        summary = "Busca todas pautas",
+        tags = { TAG_NAME },
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Operação bem sucedida",
+                content = @Content( 
+                            schema = @Schema( implementation = Pauta.class ),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE
+                        )
+            )
+        }
+    )
+    public ResponseEntity<List<Pauta>> getPautas();
+
+    @Operation(
+        operationId = "cadastra pauta",
+        summary = "Cadastra nova pauta",
+        tags = { TAG_NAME },
+        responses = {
+            @ApiResponse(
+                responseCode = "201",
+                description = "Operação bem sucedida",
+                content = @Content( 
+                            schema = @Schema( implementation = Pauta.class ),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE
+                        )
+            ),
+            @ApiResponse(
+                responseCode = "400",
+                description = "Dados enviados inválidos, confira documentação",
+                content = @Content( 
+                            schema = @Schema( implementation = Error.class ),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE
+                        )
+            )
+        }
+    )
+    public ResponseEntity<Pauta> createPauta( @RequestBody Pauta pauta );
+}
